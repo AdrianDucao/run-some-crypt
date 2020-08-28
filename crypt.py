bb523dc0c8cc2396458a9860c27f8af5c0237998
AdrianDucao <adrianducao@gmail.com>
2020-08-28 08:07:39 +0800

    generating encryption key

--- crypt.py
+++ crypt.py
@@ -6,8 +6,13 @@ class Encrypt:
 
     def __init__(self):
 
-    self.key = None
-    self.cryptor = None
-    self.file_ext_targets = ['pdf','doc','docx','ppt','pptx','txt']
+        self.key = None
+        self.cryptor = None
+        self.file_ext_targets = ['pdf','doc','docx','ppt','pptx','txt']
+
+    def generateKey(self):
+    
+        self.key = Fernet.generate_key()
+        self.cryptor = Fernet(self.key)
 
 
